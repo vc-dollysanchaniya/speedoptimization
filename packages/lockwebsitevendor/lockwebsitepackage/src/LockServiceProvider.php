acf02dff3145951489cafaccf02321de94273d50
vc-dollysanchaniya <dolly.sanchaniya@viitor.cloud>
2022-09-23 17:01:13 +0530

    serviceprovider changes

---
+++ packages/lockwebsitevendor/lockwebsitepackage/src/LockServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Lockwebsitevendor\Lockwebsitepackage;
+
+use Illuminate\Support\Facades\Route ;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\Router;
+
+class LockServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(Router $router)
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/routes.php');
+        $this->loadViewsFrom(__DIR__.'/views', 'role');
+      
+            $router->middlewareGroup('web', array(
+                \Lockwebsitevendor\Lockwebsitepackage\Http\Middleware\RouteAccess::class,
+            )
+        );
+    }
+}
