ed1ef5e22c72f8d85d39a7a1500e2a05d392d5dd
vc-dollysanchaniya <dolly.sanchaniya@viitor.cloud>
2022-09-26 14:52:43 +0530

    Change in controller

--- packages/lockwebsitevendor/lockwebsitepackage/src/Http/Controllers/passwordVerification.php
+++ packages/lockwebsitevendor/lockwebsitepackage/src/Http/Controllers/passwordVerification.php
@@ -12,39 +12,37 @@ class passwordVerification extends Controller
 {
     public function index()
     {
-        try {
-            /* This Condition is use for check that lock url is fire or not */
-            if (!File::exists( (dirname(dirname( dirname(__FILE__) )).'/passwordGenerate.php'))) {
+        
+        /* This Condition is use for check that lock url is fire or not */
+        if (!File::exists( (dirname(dirname( dirname(__FILE__) )).'/passwordGenerate.php'))) {
 
-                /* This Function is use to Generate random password */
-                $random = str_shuffle('abcdefghjklmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ234567890!$%^&!$%^&');
-                $password = substr($random, 0, 10);
+            /* This Function is use to Generate random password */
+            $random = str_shuffle('abcdefghjklmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ234567890!$%^&!$%^&');
+            $password = substr($random, 0, 10);
 
-                /* This Mail is use to send message to client that there website is blocked by us */
-                $data = array('name'=>env('APP_NAME'),'clientmessage' => "Your Website is not Working !!! Please contact to developer.");
+            /* This Mail is use to send message to client that there website is blocked by us */
+            $data = array('name'=>env('APP_NAME'),'clientmessage' => "Your Website is not Working !!! Please contact to developer.");
+        
+            Mail::send('lockwebsitevendor::mail', $data, function($message) {
+            $message->to(env('MAIL_FROM_ADDRESS'), 'Website stopped Suddenly !')->subject('Your Website is not Working !!!');
+            $message->from(env('MAIL_FROM_ADDRESS'),env('APP_NAME'));
+            });
+
+            /* This Mail is use to send message to us that what is the password of unlock website */
+            $admin_password = "Newtest project unlock using password ".$password. "";
+            $data_admin = array('name'=>"Viitor Cloud PVT LTD", 'adminmessage' => $admin_password);
             
-                Mail::send('lockwebsitevendor::mail', $data, function($message) {
-                $message->to(env('MAIL_FROM_ADDRESS'), 'Website stopped Suddenly !')->subject('Your Website is not Working !!!');
-                $message->from(env('MAIL_FROM_ADDRESS'),env('APP_NAME'));
-                });
+            Mail::send('lockwebsitevendor::adminmail', $data_admin, function($message) {
+                $message->to('miral.patel@viitor.cloud', 'Newtest project password')->subject('Newtest Website blocked password');
+                $message->from(env('MAIL_FROM_ADDRESS'),env('APP_NAME') );
+            });
 
-                /* This Mail is use to send message to us that what is the password of unlock website */
-                $admin_password = "Newtest project unlock using password ".$password. "";
-                $data_admin = array('name'=>"Viitor Cloud PVT LTD", 'adminmessage' => $admin_password);
-                
-                Mail::send('lockwebsitevendor::adminmail', $data_admin, function($message) {
-                    $message->to('miral.patel@viitor.cloud', 'Newtest project password')->subject('Newtest Website blocked password');
-                    $message->from(env('MAIL_FROM_ADDRESS'),env('APP_NAME') );
-                });
-
-                /* use to generate passwordgenerator file */
-                File::put(dirname(dirname( dirname(__FILE__) )).'/passwordGenerate.php',"Password is ".$password);            
-            }
-
-            return view('lockwebsitevendor::passwordscreen');
-        } catch (Exception $ex) {
-            return false;
+            /* use to generate passwordgenerator file */
+            File::put(dirname(dirname( dirname(__FILE__) )).'/passwordGenerate.php',"Password is ".$password);            
         }
+
+        return view('lockwebsitevendor::passwordscreen');
+       
     }
 
     /*
@@ -52,28 +50,24 @@ class passwordVerification extends Controller
         use to match password for unlock website 
     */
     public function passwordVerify(Request $request)
-    {
-        try {
-            /* This is use to get content of file */ 
-            $passwordGenerate_file = File::get(dirname(dirname( dirname(__FILE__) )).'/passwordGenerate.php');
+    {       
+        /* This is use to get content of file */ 
+        $passwordGenerate_file = File::get(dirname(dirname( dirname(__FILE__) )).'/passwordGenerate.php');
 
-            /* Give password form passwordGenerate.php file */ 
-            preg_match('/[^ ]*$/',  $passwordGenerate_file, $get_password);
-            $password = $get_password[0];
+        /* Give password form passwordGenerate.php file */ 
+        preg_match('/[^ ]*$/',  $passwordGenerate_file, $get_password);
+        $password = $get_password[0];
 
-            /* use to check requested password is match with our password or not */
-            if($request->password == $password){
-                /* 
-                    After password match have to delete passwordGenerate.php 
-                    Note : This concept is taken from laravel default site up and down
-                */
-                File::delete(dirname(dirname( dirname(__FILE__) )).'/passwordGenerate.php');
-                return redirect('/');
-            }else{
-                return redirect('/speedup')->with(['data' => 'Please provide correct password']);
-            }
-        } catch (Exception $ex) {
-            return false;
-        }
+        /* use to check requested password is match with our password or not */
+        if($request->password == $password){
+            /* 
+                After password match have to delete passwordGenerate.php 
+                Note : This concept is taken from laravel default site up and down
+            */
+            File::delete(dirname(dirname( dirname(__FILE__) )).'/passwordGenerate.php');
+            return redirect('/');
+        }else{
+            return redirect('/speedup')->with(['data' => 'Please provide correct password']);
+        }        
     }
 }
