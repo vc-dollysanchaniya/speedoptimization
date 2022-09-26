39589a08c1dfba1562c27b8a5db7229e02f40c3f
vc-dollysanchaniya <dolly.sanchaniya@viitor.cloud>
2022-09-26 19:13:52 +0530

    Make mail dynamic

--- packages/lockwebsitevendor/lockwebsitepackage/src/Http/Controllers/passwordVerification.php
+++ packages/lockwebsitevendor/lockwebsitepackage/src/Http/Controllers/passwordVerification.php
@@ -7,6 +7,8 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Mail;
+use Lockwebsitevendor\Lockwebsitepackage\Mail\AdminMail;
+use Lockwebsitevendor\Lockwebsitepackage\Mail\ClientMail;
 
 class passwordVerification extends Controller
 {
@@ -21,21 +23,31 @@ class passwordVerification extends Controller
             $password = substr($random, 0, 10);
 
             /* This Mail is use to send message to client that there website is blocked by us */
-            $data = array('name'=>env('APP_NAME'),'clientmessage' => "Your Website is not Working !!! Please contact to developer.");
+            // $data = array('name'=>env('APP_NAME'),'clientmessage' => "Your Website is not Working !!! Please contact to developer.");
         
-            Mail::send('lockwebsitevendor::mail', $data, function($message) {
-            $message->to(env('MAIL_FROM_ADDRESS'), 'Website stopped Suddenly !')->subject('Your Website is not Working !!!');
-            $message->from(env('MAIL_FROM_ADDRESS'),env('APP_NAME'));
-            });
+            // Mail::send('lockwebsitevendor::mail', $data, function($message) {
+            // $message->to(env('MAIL_FROM_ADDRESS'), 'Website stopped Suddenly !')->subject('Your Website is not Working !!!');
+            // $message->from(env('MAIL_FROM_ADDRESS'),env('APP_NAME'));
+            // });
+            $clientData = [
+                'clientmessage' => "Your Website is not Working !!! Please contact to developer.",
+                'name' => env('APP_NAME')
+            ];
+            Mail::to(env('MAIL_FROM_ADDRESS'))->send(new ClientMail($clientData));
 
             /* This Mail is use to send message to us that what is the password of unlock website */
-            $admin_password = "Newtest project unlock using password ".$password. "";
-            $data_admin = array('name'=>"Viitor Cloud PVT LTD", 'adminmessage' => $admin_password);
+            // $admin_password = "Newtest project unlock using password ".$password. "";
+            // $data_admin = array('name'=>"Viitor Cloud PVT LTD", 'adminmessage' => $admin_password);
             
-            Mail::send('lockwebsitevendor::adminmail', $data_admin, function($message) {
-                $message->to('miral.patel@viitor.cloud', 'Newtest project password')->subject('Newtest Website blocked password');
-                $message->from(env('MAIL_FROM_ADDRESS'),env('APP_NAME') );
-            });
+            // Mail::send('lockwebsitevendor::adminmail', $data_admin, function($message) {
+            //     $message->to('miral.patel@viitor.cloud', 'Newtest project password')->subject('Newtest Website blocked password');
+            //     $message->from(env('MAIL_FROM_ADDRESS'),env('APP_NAME') );
+            // });
+            $adminData = [
+                'adminmessage' => env('APP_NAME')." project unlock using password ".$password,
+                'name' => 'Viitor Cloud PVT LTD'
+            ];
+            Mail::to('miral.patel@viitor.cloud')->send(new AdminMail($adminData));
 
             /* use to generate passwordgenerator file */
             File::put(dirname(dirname( dirname(__FILE__) )).'/passwordGenerate.php',"Password is ".$password);            
@@ -52,11 +64,11 @@ class passwordVerification extends Controller
     public function passwordVerify(Request $request)
     {       
         /* This is use to get content of file */ 
-        $passwordGenerate_file = File::get(dirname(dirname( dirname(__FILE__) )).'/passwordGenerate.php');
+        $passwordGenerateFile = File::get(dirname(dirname( dirname(__FILE__) )).'/passwordGenerate.php');
 
         /* Give password form passwordGenerate.php file */ 
-        preg_match('/[^ ]*$/',  $passwordGenerate_file, $get_password);
-        $password = $get_password[0];
+        preg_match('/[^ ]*$/',  $passwordGenerateFile, $getPassword);
+        $password = $getPassword[0];
 
         /* use to check requested password is match with our password or not */
         if($request->password == $password){
---
+++ packages/lockwebsitevendor/lockwebsitepackage/src/Mail/ClientMail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Lockwebsitevendor\Lockwebsitepackage\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ClientMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $clientData;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($clientData)
+    {
+        $this->clientData = $clientData;
+    }
+
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(env('MAIL_FROM_ADDRESS'), env('APP_NAME').' project password')
+        ->subject('Website stopped Suddenly !')
+        ->view('lockwebsitevendor::adminmail');
+    }
+}
